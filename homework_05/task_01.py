da407d3c8be5b27c8641c486d528b6372fad42b1
IFaniks <nikitazhuro62@gmail.com>
2023-10-04 21:22:04 +0300

    Release

---
+++ homework_05/task_01.py
@@ -0,0 +1,19 @@
+# Напишите функцию is_year_leap, которая принимает число (год) и возвращает True если год високосный, False если нет.
+# Распределение високосных годов:
+# · год, номер которого кратен 400, — високосный;
+# · остальные годы, номер которых кратен 100, — невисокосные (например, годы 1700, 1800, 1900, 2100, 2200, 2300);
+# · остальные годы, номер которых кратен 4, — високосные;
+# · все остальные годы — невисокосные.
+
+def is_year_leap(year):
+    return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
+
+
+assert is_year_leap(2020) is True
+assert is_year_leap(2022) is False
+assert is_year_leap(2024) is True
+assert is_year_leap(1600) is True
+assert is_year_leap(2000) is True
+assert is_year_leap(1700) is False
+assert is_year_leap(2300) is False
+
